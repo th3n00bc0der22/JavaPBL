e9ce5425ad4d9a7d45e7ebf229ff22c9fe5d7d65
BlueKyouma <dogpolar22@gmail.com>
2026-09-06 00:53:39 -0300

    menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+
+import java.util.List;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,10 +10,50 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int option;
+        option = openMenu();
+        while(!List.of(1,2,3).contains(option))
+           option = openMenu();
+
+    }
+
+
+    public static int openMenu(){
+        limpaTerminal();
+        Scanner scanner = new Scanner(System.in);
+        
+
+        System.out.println("digite o número da opção desejada");
+        System.out.println("------Menu principal-------");
+        System.out.println("Opção 1 - Jogar");
+        System.out.println("Opção 2 - Créditos");
+        System.out.println("Opção 3 - sair");
+        
+        try{
+
+        return scanner.nextInt();
+        }
+        catch(Exception e){
+            System.out.println("entrada inválida");
+            return 0;
+        }
+
+    }
+
+    public static void limpaTerminal(){
+        try {
+            String SO = System.getProperty("os.name");
+
+            if (SO.contains("Windows")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+            else{
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+
+        } catch (Exception e) {
+            System.out.println("terminal não limpo, excessão: " + e);
         }
     }
+
 }
